ee52c155aa03d7118ff178bee7988045fe88e7f9
darksun <lujun9972@gmail.com>
2017-04-09 11:33:37 +0800

    update

--- html2org.el
+++ html2org.el
@@ -3,29 +3,6 @@
 (require 'shr)
 (require 'subr-x)
 
-(defgroup html2org nil
-  "Save http(s) page as org file")
-
-(defcustom html2org-store-dir "/home/lujun9972/github/emacs-document/raw/"
-  "The directory to store org files"
-  :type 'directory)
-
-(defcustom html2org-timeout 30
-  "Timeout seconds"
-  :type 'number)
-
-(defun html2org-get-dom (&optional url)
-  "Retrive `URL' and return the dom."
-  (let* ((url (or url (read-string "url: ")))
-         (buf (with-timeout
-                  (html2org-timeout
-                   (error "fetch %s failed in %d seconds" url html2org-timeout))
-                (url-retrieve-synchronously url))))
-    (prog1 (ignore-errors (with-current-buffer buf
-                            (goto-char url-http-end-of-headers)
-                            (libxml-parse-html-region (point) (point-max) url t)))
-      (kill-buffer buf))))
-
 (defun html2org-tag-a (dom)
   "Convert DOM into org-mode style link."
   (let ((url (dom-attr dom 'href))
@@ -45,35 +22,56 @@
           (insert (format "[[%s]]" url))
         (insert (format "[[%s][%s]]" url description))))))
 
+(defun html2org-fontize-dom (dom type)
+  (unless (or (looking-back "[[:blank:]]")
+              (save-excursion
+                (= (point) (progn (beginning-of-line)
+                                  (point)))))
+    (insert " "))
+  (insert type)
+  (shr-generic dom)
+  (insert type)
+  (unless (looking-at-p "[[:blank:]]")
+    (insert " ")))
+
+(defun html2org-tag-b (dom)
+  (html2org-fontize-dom dom "*"))
+
+(defun html2org-tag-i (dom)
+  (html2org-fontize-dom dom "/"))
+
+(defun html2org-tag-em (dom)
+  (html2org-fontize-dom dom "/"))
+
+(defun html2org-tag-strong (dom)
+  (html2org-fontize-dom dom "*"))
+
+(defun html2org-tag-u (dom)
+  (html2org-fontize-dom dom "_"))
+
 (defun html2org-transform-dom (dom)
   "Transform DOM into org file content."
-  (let ((shr-external-rendering-functions '((a . html2org-tag-a))))
+  (let ((shr-external-rendering-functions '((a . html2org-tag-a)
+                                            (b . html2org-tag-b)
+                                            (i . html2org-tag-i)
+                                            (em . html2org-tag-em)
+                                            (strong . html2org-tag-strong)
+                                            (u . html2org-tag-u))))
     (with-temp-buffer
       (shr-insert-document dom)
-      (replace-regexp-in-string "^\\*" ",*" (buffer-string)))))
+      (replace-regexp-in-string "^\\(\\*[[:blank:]]+\\)" ",\\1" (buffer-string)))))
 
-;; (with-current-buffer "*eww*"
-;;   (html2org (eww-current-url) nil :TITLE "title" :TAGS "tag1 tags"))
 
-;;;###autoload
-(defun html2org (&optional url org-file &rest option-plist)
-  "Retrive URL and write the content into ORG-FILE with org-mode style links.
 
-This function will return the saved ORG-FILE path"
+(defun html2org (&optional buf)
+  "Convert HTML to org text in the BUF"
   (interactive)
-  (let* ((url (or url (read-string "url: ")))
-         (dom (html2org-get-dom url))
-         (title (dom-text (dom-by-tag dom 'title)))
-         (org-file (or org-file (expand-file-name (concat title ".org") html2org-store-dir)))
-         (content (html2org-transform-dom dom)))
-    (with-temp-file org-file
-      (while option-plist
-        (let ((option (format "%s" (pop option-plist)))
-              (value (format "%s" (pop option-plist))))
-          (when (string-prefix-p ":" option)
-            (setq option (substring option 1))) ;handler format like :option
-          (insert (format "#+%s: %s\n" option value))))
-      (insert content))
-    org-file))
+  (let ((buf (or buf (current-buffer)))
+        (shr-external-rendering-functions '((a . html2org-tag-a))))
+    (with-current-buffer buf
+      (let ((dom (libxml-parse-html-region (point-min) (point-max))))
+        (erase-buffer)
+        (insert (html2org-transform-dom dom))))))
+
 
 (provide 'html2org)
